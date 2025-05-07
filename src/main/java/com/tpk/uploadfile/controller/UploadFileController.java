769dda4210e8e47b9e63bf09e19e92223cba80ab
taknaja <peerapolkoeram@gmail.com>
2025-05-07 19:30:24 +0700

    delete import

--- src/main/java/com/tpk/uploadfile/controller/UploadFileController.java
+++ src/main/java/com/tpk/uploadfile/controller/UploadFileController.java
@@ -12,13 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 @Controller
